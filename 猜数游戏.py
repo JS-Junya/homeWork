e7b6c2aa99bd0ce88ef4ad44507f47788611a93f
JS_Jun <40715347+JS-Junya@users.noreply.github.com>
2023-04-06 16:51:53 +0800

    Add files via upload

---
+++ 猜数游戏.py
@@ -0,0 +1,11 @@
+setNum = 6
+for i in range(10):
+    inNum = eval(input("请输入一个整数："))
+    print("输入错误")
+    if inNum < setNum:
+        print("遗憾，太小了")
+    elif inNum > setNum:
+        print("遗憾，太大了")
+    else:
+        print("预测{}次，你猜中了！".format(i+1))
+        break
